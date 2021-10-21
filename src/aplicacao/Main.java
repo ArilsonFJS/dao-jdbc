d9a86cc35827e0ed06647e00259b3f2044aa254b
ArilsonFJS <arilsonsilva61@gmail.com>
2021-10-21 14:21:28 -0300

    add metodo findByDepartment

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,16 +1,29 @@
 package aplicacao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entidades.Department;
 import model.entidades.Seller;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		SellerDao sellerDao	= DaoFactory.createSellerDao();
-		
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		Seller seller = sellerDao.findById(3);
-		
+
 		System.out.println(seller);
+
+		System.out.println("===== teste findByDepartment=======");
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
