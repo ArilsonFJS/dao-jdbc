a218496eea4da311d571a7f4e71732d9cc53e652
ArilsonFJS <arilsonsilva61@gmail.com>
2021-10-21 16:15:48 -0300

    implementado update

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -39,5 +39,11 @@ public class Main {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Insert! new Id = " + newSeller.getId());
+		
+		System.out.println("\n===== Editar=======");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update complete");
 	}
 }
