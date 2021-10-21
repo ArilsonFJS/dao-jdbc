e9435aa596bdb5ffee27b68ad6999cb1d0e7f2fc
ArilsonFJS <arilsonsilva61@gmail.com>
2021-10-21 16:26:08 -0300

    implementado delete

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -2,6 +2,7 @@ package aplicacao;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,7 @@ import model.entidades.Seller;
 public class Main {
 	public static void main(String[] args) {
 
+		Scanner scn = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		Seller seller = sellerDao.findById(3);
@@ -33,17 +35,25 @@ public class Main {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n===== Inserir=======");
-		
+
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Insert! new Id = " + newSeller.getId());
-		
+
 		System.out.println("\n===== Editar=======");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update complete");
+
+		System.out.println("\n=====Delete=======");
+		System.out.println("Entre com id para delecao");
+		int id = scn.nextInt();
+		sellerDao.deleteById(id);
+
+		System.out.println("Delete complete!");
+		scn.close();
 	}
 }
